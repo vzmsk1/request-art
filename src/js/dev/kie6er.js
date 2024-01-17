e0f659c66948504998303be4b250f73c07cb9b8d
Kie6er <kirill.zawgorodny@yandex.ru>
2024-01-17 14:27:17 +0300

    fix header && footer

--- src/js/dev/kie6er.js
+++ src/js/dev/kie6er.js
@@ -28,32 +28,4 @@ $(document).ready(function () {
             }
         }
     }
-
-    $('.burger-btn').on('click', function () {
-        $(this).toggleClass('open');
-        $('.burger-menu').toggleClass('show');
-        $('body').toggleClass('lock');
-    });
-
-    scrollHeader();
-    $(window).resize(function () {
-        if ($(this).innerWidth() >= 769) {
-            $('body').removeClass('lock');
-            $('.burger-btn').removeClass('open');
-            $('.burger-menu').removeClass('show');
-        }
-
-        scrollHeader();
-    });
-    $(window).on('scroll', scrollHeader);
-
-    function scrollHeader() {
-        if ($(window).innerWidth() < 769) {
-            if ($(window).scrollTop() > 1) {
-                $('.header').addClass('scroll');
-            } else {
-                $('.header').removeClass('scroll');
-            }
-        }
-    }
 });
