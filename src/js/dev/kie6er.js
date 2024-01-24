28f9e8d21d277128fd5a7c028f3c11a0a2221792
Kie6er <kirill.zawgorodny@yandex.ru>
2024-01-24 20:47:30 +0300

    main-page

--- src/js/dev/kie6er.js
+++ src/js/dev/kie6er.js
@@ -48,4 +48,52 @@ $(document).ready(function () {
             }
         }
     });
+
+    const advantagesSwiper = new Swiper('.main-advantages__cards', {
+        enabled: true,
+        slidesPerView: 'auto',
+        spaceBetween: `${remToPx(2.4)}rem`,
+        speed: 500,
+        breakpoints: {
+            768: {
+                spaceBetween: 0,
+                enabled: false
+            }
+        }
+    });
+
+    const conceptSwiper = new Swiper('.main-concept__content', {
+        enabled: true,
+        slidesPerView: 'auto',
+        spaceBetween: `${remToPx(2.4)}rem`,
+        speed: 500,
+        breakpoints: {
+            768: {
+                spaceBetween: 0,
+                enabled: false
+            }
+        }
+    });
+
+    if ($(window).outerWidth() > 768) {
+        $('.main-concept__left').on('mouseover', function () {
+            $(this).addClass('hover');
+            $('.main-concept').css('background-color', '#FAD85D');
+        });
+
+        $('.main-concept__left').on('mouseleave', function () {
+            $(this).removeClass('hover');
+            $('.main-concept').css('background', 'none');
+        });
+
+        $('.main-concept__right').on('mouseover', function () {
+            $(this).addClass('hover');
+            $('.main-concept').css('background-color', '#CAD2F4');
+        });
+
+        $('.main-concept__right').on('mouseleave', function () {
+            $(this).removeClass('hover');
+            $('.main-concept').css('background', 'none');
+        });
+    }
 });
