8cee65ebc5ae182fc161e5d04aafdece54ae7414
vyazemskaya <vyazemskayanna@gmail.com>
2024-01-25 19:18:39 +0300

    update: portfolio page

--- src/js/components/masonry.js
+++ src/js/components/masonry.js
@@ -1,11 +1,20 @@
 import Masonry from 'masonry-layout';
 
-export const msnry = new Masonry('.portfolio__cards', {
-    // options...
-    itemSelector: '.portfolio__card',
-    columnWidth: '.grid-sizer',
-    gutter: '.gutter-sizer',
-    horizontalOrder: true,
-    percentPosition: true,
-    initLayout: window.innerWidth <= 768 ? true : false
-});
+export const msnry = document.querySelector('.portfolio__cards')
+    ? new Masonry('.portfolio__cards', {
+          // options...
+          itemSelector: '.portfolio__card',
+          columnWidth: '.grid-sizer',
+          gutter: '.gutter-sizer',
+          horizontalOrder: true,
+          percentPosition: true,
+          initLayout: window.innerWidth <= 768 ? true : false
+      })
+    : null;
+
+if (msnry) {
+    msnry.on('removeComplete', function (removedItems) {
+        removeAttribute(document.querySelector('.portfolio__cards'), 'style');
+        removeAttribute(document.querySelectorAll('.portfolio__card'), 'style');
+    });
+}
